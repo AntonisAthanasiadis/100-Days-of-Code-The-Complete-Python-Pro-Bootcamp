3189383d8185f9a24d841bd87aa147c65db95c33
Antonis Athanasiadis <antonis.athanasiadis@outlook.com>
2026-03-03 12:04:44 +0200

    Completed chapters about number manipulation including rounding and type conversion, and f-strings.

---
+++ variables_data_types/number_manipulation.py
@@ -0,0 +1,16 @@
+#Rounding bmi result
+bmi = 84 / 1.65 ** 2
+print(bmi)
+
+#Convert to int, deletes decimal part
+print(int(bmi))
+#Rounds at nearest int
+print(round(bmi))
+#Rounds at 2 decimal
+print(round(bmi, 2))
+
+#Add/Multiply etc shortcut
+score = 0
+#User scores a point
+score +=1
+print(score)
